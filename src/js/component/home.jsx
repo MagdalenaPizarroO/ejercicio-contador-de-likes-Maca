d9a512b5565f2680e949c3ead2685d9645784b5a
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-03-20 01:43:46 +0000

    estudiando hooks, en proceso

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,20 +5,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let likes = 0;
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>{likes}</h1>
+			<button
+				onClick={()=>{
+					likes+=1
+					console.log(likes)
+				}}
+			>+1</button>
 		</div>
 	);
 };
